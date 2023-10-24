0323e07de2c7ee55201eb23a3a4ebad2109dd034
Jeff Bencin <jeff.bencin@gmail.com>
2023-10-24 12:47:07 -0400

    Refactor unit tests for `lib.ts`

--- tests/lib.test.ts
+++ tests/lib.test.ts
@@ -61,39 +61,41 @@ test('Multisig address generation', () => {
   expect(c32_address).toEqual(c32_expected);
 });
 
-test('Multisig address validation (success)', () => {
-  //const address = C32.c32address(StxTx.AddressVersion.MainnetMultiSig, "b01162ecda72c57ed419f7966ec4e8dd7987c704");
-  const address = "SM2R12RQCV9SCAZPM37VSCVP4X3EQK1Y70KCV7EDE";
-  const pubkeys = [
-    "02b30fafab3a12372c5d150d567034f37d60a91168009a779498168b0e9d8ec7f2", // 1
-    "03ce61f1d155738a5e434fc8a61c3e104f891d1ec71576e8ad85abb68b34670d35", // 2
-    "03ef2340518b5867b23598a9cf74611f8b98064f7d55cdb8c107c67b5efcbc5c77", // 3
-  ];
-  lib.checkAddressPubKeyMatch(pubkeys, 2, address);
+describe('Multisig address validation', () => {
+  it('Should return valid for pubkeys in any order', () => {
+    //const address = C32.c32address(StxTx.AddressVersion.MainnetMultiSig, "b01162ecda72c57ed419f7966ec4e8dd7987c704");
+    const address = "SM2R12RQCV9SCAZPM37VSCVP4X3EQK1Y70KCV7EDE";
+    const pubkeys = [
+      "02b30fafab3a12372c5d150d567034f37d60a91168009a779498168b0e9d8ec7f2", // 1
+      "03ce61f1d155738a5e434fc8a61c3e104f891d1ec71576e8ad85abb68b34670d35", // 2
+      "03ef2340518b5867b23598a9cf74611f8b98064f7d55cdb8c107c67b5efcbc5c77", // 3
+    ];
+    lib.checkAddressPubKeyMatch(pubkeys, 2, address);
 
-  // Should work with wrong ordering also
-  const pubkeysWrongOrder = [
-    "03ce61f1d155738a5e434fc8a61c3e104f891d1ec71576e8ad85abb68b34670d35", // 2
-    "03ef2340518b5867b23598a9cf74611f8b98064f7d55cdb8c107c67b5efcbc5c77", // 3
-    "02b30fafab3a12372c5d150d567034f37d60a91168009a779498168b0e9d8ec7f2", // 1
-  ];
-  const pubkeysReordered = lib.checkAddressPubKeyMatch(pubkeysWrongOrder, 2, address);
+    // Should work with wrong ordering also
+    const pubkeysWrongOrder = [
+      "03ce61f1d155738a5e434fc8a61c3e104f891d1ec71576e8ad85abb68b34670d35", // 2
+      "03ef2340518b5867b23598a9cf74611f8b98064f7d55cdb8c107c67b5efcbc5c77", // 3
+      "02b30fafab3a12372c5d150d567034f37d60a91168009a779498168b0e9d8ec7f2", // 1
+    ];
+    const pubkeysReordered = lib.checkAddressPubKeyMatch(pubkeysWrongOrder, 2, address);
 
-  // Should return keys in order used to generate address
-  expect(pubkeysReordered).toEqual(pubkeys);
+    // Should return keys in order used to generate address
+    expect(pubkeysReordered).toEqual(pubkeys);
+  });
+
+  it('Should fail if passed wrong number of signatures required', () => {
+    //const address = C32.c32address(StxTx.AddressVersion.MainnetMultiSig, "b01162ecda72c57ed419f7966ec4e8dd7987c704");
+    const address = "SM2R12RQCV9SCAZPM37VSCVP4X3EQK1Y70KCV7EDE";
+    const pubkeys = [
+      "02b30fafab3a12372c5d150d567034f37d60a91168009a779498168b0e9d8ec7f2", // 1
+      "03ce61f1d155738a5e434fc8a61c3e104f891d1ec71576e8ad85abb68b34670d35", // 2
+      "03ef2340518b5867b23598a9cf74611f8b98064f7d55cdb8c107c67b5efcbc5c77", // 3
+
+    ];
+    expect(() => lib.checkAddressPubKeyMatch(pubkeys, 1, address)).toThrowError();
+  });
 });
-
-test('Multisig address validation (failure)', () => {
-  //const address = C32.c32address(StxTx.AddressVersion.MainnetMultiSig, "b01162ecda72c57ed419f7966ec4e8dd7987c704");
-  const address = "SM2R12RQCV9SCAZPM37VSCVP4X3EQK1Y70KCV7EDE";
-  const pubkeys = [
-    "02b30fafab3a12372c5d150d567034f37d60a91168009a779498168b0e9d8ec7f2", // 1
-    "03ce61f1d155738a5e434fc8a61c3e104f891d1ec71576e8ad85abb68b34670d35", // 2
-    "03ef2340518b5867b23598a9cf74611f8b98064f7d55cdb8c107c67b5efcbc5c77", // 3
-  ];
-  expect(() => lib.checkAddressPubKeyMatch(pubkeys, 1, address)).toThrowError();
-});
-
 
 test('Get auth field info', async () => {
   const tx = await lib.generateMultiSignedTx();
@@ -150,113 +152,115 @@ describe('getSignersAfter()', () => {
   });
 });
 
-describe('Transaction building (success)', async () => {
-  const recipient = 'ST2ZRX0K27GW0SP3GJCEMHD95TQGJMKB7G9Y0X1MH';
-  const publicKeys = [
-    "02b30fafab3a12372c5d150d567034f37d60a91168009a779498168b0e9d8ec7f2", // 1
-    "03ce61f1d155738a5e434fc8a61c3e104f891d1ec71576e8ad85abb68b34670d35", // 2
-    "03ef2340518b5867b23598a9cf74611f8b98064f7d55cdb8c107c67b5efcbc5c77", // 3
-  ];
-  const data: lib.MultisigTxInput = {
-    recipient, fee: '300', amount: '10000', publicKeys, numSignatures: 3, nonce: '4', network: 'mainnet'
-  };
+describe('Transaction building', async () => {
+  describe('From valid inputs', async () => {
+    const recipient = 'ST2ZRX0K27GW0SP3GJCEMHD95TQGJMKB7G9Y0X1MH';
+    const publicKeys = [
+      "02b30fafab3a12372c5d150d567034f37d60a91168009a779498168b0e9d8ec7f2", // 1
+      "03ce61f1d155738a5e434fc8a61c3e104f891d1ec71576e8ad85abb68b34670d35", // 2
+      "03ef2340518b5867b23598a9cf74611f8b98064f7d55cdb8c107c67b5efcbc5c77", // 3
+    ];
+    const data: lib.MultisigTxInput = {
+      recipient, fee: '300', amount: '10000', publicKeys, numSignatures: 3, nonce: '4', network: 'mainnet'
+    };
 
-  const tx = await lib.makeStxTokenTransfer(data);
-  const spendingCondition = tx.auth.spendingCondition as StxTx.MultiSigSpendingCondition;
+    const tx = await lib.makeStxTokenTransfer(data);
+    const spendingCondition = tx.auth.spendingCondition as StxTx.MultiSigSpendingCondition;
 
-  it('Should have correct numbers of auth fields', () => {
-    const info = lib.getAuthFieldInfo(tx);
-    expect(info).toEqual({
-      authFields: 3,
-      pubkeys: 3,
-      signatures: 0,
-      signaturesRequired: 3,
-    });
-  });
-
-  it('Should have correct pubkeys', () => {
-    spendingCondition.fields.forEach((f, i) => {
-      expect(f.contents.type).toEqual(StxTx.StacksMessageType.PublicKey);
-      const pubkey = f.contents.data.toString('hex');
-      expect(pubkey).toEqual(publicKeys[i]);
-    });
-  });
-
-  it('Should have correct fee, nonce, and hash mode', () => {
-    expect(spendingCondition.fee).toEqual(300n);
-    expect(spendingCondition.nonce).toEqual(4n);
-    expect(spendingCondition.hashMode).toEqual(StxTx.AddressHashMode.SerializeP2SH);
-  });
-});
-
-test('Transaction building (failure): Invalid `sender`', async () => {
-  const sender = 'ST2ZRX0K27GW0SP3GJCEMHD95TQGJMKB7G9Y0X1MH'; // Invalid
-  const recipient = 'ST2ZRX0K27GW0SP3GJCEMHD95TQGJMKB7G9Y0X1MH';
-  const publicKeys = [
-    "02b30fafab3a12372c5d150d567034f37d60a91168009a779498168b0e9d8ec7f2", // 1
-    "03ce61f1d155738a5e434fc8a61c3e104f891d1ec71576e8ad85abb68b34670d35", // 2
-    "03ef2340518b5867b23598a9cf74611f8b98064f7d55cdb8c107c67b5efcbc5c77", // 3
-  ];
-  const data: lib.MultisigTxInput = {
-    sender, recipient, fee: '300', amount: '10000', publicKeys, numSignatures: 3, nonce: '4', network: 'mainnet'
-  };
-
-  await expect(() => lib.makeStxTokenTransfer(data)).rejects.toThrowError(/not match/);
-});
-
-describe('Transaction building from array (success)', async () => {
-  const sender = 'SM2R12RQCV9SCAZPM37VSCVP4X3EQK1Y70KCV7EDE'; // This should match signers
-  const recipient = 'ST2ZRX0K27GW0SP3GJCEMHD95TQGJMKB7G9Y0X1MH';
-  const publicKeys = [
-    "02b30fafab3a12372c5d150d567034f37d60a91168009a779498168b0e9d8ec7f2", // 1
-    "03ce61f1d155738a5e434fc8a61c3e104f891d1ec71576e8ad85abb68b34670d35", // 2
-    "03ef2340518b5867b23598a9cf74611f8b98064f7d55cdb8c107c67b5efcbc5c77", // 3
-  ];
-
-  const inputs: lib.MultisigTxInput[] = [
-    { recipient, fee: '300', amount:  '10000', publicKeys, numSignatures: 3, nonce: '4', network: 'testnet' },
-    { recipient, fee: '777', amount: '100000', publicKeys, numSignatures: 2, network: 'testnet' }, // Should work without `nonce`
-    { recipient, fee: '300', amount:  '50000', publicKeys, numSignatures: 1, nonce: '1' }, // Should work without `network`
-    { recipient, fee: '777', amount: '100000', publicKeys, numSignatures: 2, sender }, // Should work with `sender`
-  ];
-
-  const txs = await lib.makeStxTokenTransfers(inputs);
-  const expectedTxsLen = inputs.length;
-
-  it(`Should have generated ${expectedTxsLen} transactions`, () => {
-    expect(txs.length).toEqual(expectedTxsLen);
-  });
-
-  for (const i in inputs) {
-    const input = inputs[i];
-    const tx = txs[i];
-    const expectedAuthFields = input.publicKeys.length;
-
-    it(`Tx ${i} should have ${expectedAuthFields} auth fields`, () => {
+    it('Should have correct numbers of auth fields', () => {
       const info = lib.getAuthFieldInfo(tx);
       expect(info).toEqual({
-        authFields: expectedAuthFields,
-        pubkeys: expectedAuthFields,
+        authFields: 3,
+        pubkeys: 3,
         signatures: 0,
-        signaturesRequired: input.numSignatures,
+        signaturesRequired: 3,
       });
     });
 
-    const spendingCondition = tx.auth.spendingCondition as StxTx.MultiSigSpendingCondition;
-    it(`Tx ${i} should have correct pubkeys`, () => {
+    it('Should have correct pubkeys', () => {
       spendingCondition.fields.forEach((f, i) => {
         expect(f.contents.type).toEqual(StxTx.StacksMessageType.PublicKey);
         const pubkey = f.contents.data.toString('hex');
-        expect(pubkey).toEqual(input.publicKeys[i]);
+        expect(pubkey).toEqual(publicKeys[i]);
       });
     });
 
-    it(`Tx ${i} should have correct fee, nonce, and hash mode`, () => {
-      expect(spendingCondition.fee).toEqual(BigInt(input.fee));
+    it('Should have correct fee, nonce, and hash mode', () => {
+      expect(spendingCondition.fee).toEqual(300n);
+      expect(spendingCondition.nonce).toEqual(4n);
       expect(spendingCondition.hashMode).toEqual(StxTx.AddressHashMode.SerializeP2SH);
-      if (input.nonce) {
-        expect(spendingCondition.nonce).toEqual(BigInt(input.nonce));
-      }
     });
-  }
+  });
+
+  it('Should fail with invalid `sender`', async () => {
+    const sender = 'ST2ZRX0K27GW0SP3GJCEMHD95TQGJMKB7G9Y0X1MH'; // Invalid
+    const recipient = 'ST2ZRX0K27GW0SP3GJCEMHD95TQGJMKB7G9Y0X1MH';
+    const publicKeys = [
+      "02b30fafab3a12372c5d150d567034f37d60a91168009a779498168b0e9d8ec7f2", // 1
+      "03ce61f1d155738a5e434fc8a61c3e104f891d1ec71576e8ad85abb68b34670d35", // 2
+      "03ef2340518b5867b23598a9cf74611f8b98064f7d55cdb8c107c67b5efcbc5c77", // 3
+    ];
+    const data: lib.MultisigTxInput = {
+      sender, recipient, fee: '300', amount: '10000', publicKeys, numSignatures: 3, nonce: '4', network: 'mainnet'
+    };
+
+    await expect(() => lib.makeStxTokenTransfer(data)).rejects.toThrowError(/not match/);
+  });
+
+  describe('From array', async () => {
+    const sender = 'SM2R12RQCV9SCAZPM37VSCVP4X3EQK1Y70KCV7EDE'; // This should match signers
+    const recipient = 'ST2ZRX0K27GW0SP3GJCEMHD95TQGJMKB7G9Y0X1MH';
+    const publicKeys = [
+      "02b30fafab3a12372c5d150d567034f37d60a91168009a779498168b0e9d8ec7f2", // 1
+      "03ce61f1d155738a5e434fc8a61c3e104f891d1ec71576e8ad85abb68b34670d35", // 2
+      "03ef2340518b5867b23598a9cf74611f8b98064f7d55cdb8c107c67b5efcbc5c77", // 3
+    ];
+
+    const inputs: lib.MultisigTxInput[] = [
+      { recipient, fee: '300', amount:  '10000', publicKeys, numSignatures: 3, nonce: '4', network: 'testnet' },
+      { recipient, fee: '777', amount: '100000', publicKeys, numSignatures: 2, network: 'testnet' }, // Should work without `nonce`
+      { recipient, fee: '300', amount:  '50000', publicKeys, numSignatures: 1, nonce: '1' }, // Should work without `network`
+      { recipient, fee: '777', amount: '100000', publicKeys, numSignatures: 2, sender }, // Should work with `sender`
+    ];
+
+    const txs = await lib.makeStxTokenTransfers(inputs);
+    const expectedTxsLen = inputs.length;
+
+    it(`Should have generated ${expectedTxsLen} transactions`, () => {
+      expect(txs.length).toEqual(expectedTxsLen);
+    });
+
+    for (const i in inputs) {
+      const input = inputs[i];
+      const tx = txs[i];
+      const expectedAuthFields = input.publicKeys.length;
+
+      it(`Tx ${i} should have ${expectedAuthFields} auth fields`, () => {
+        const info = lib.getAuthFieldInfo(tx);
+        expect(info).toEqual({
+          authFields: expectedAuthFields,
+          pubkeys: expectedAuthFields,
+          signatures: 0,
+          signaturesRequired: input.numSignatures,
+        });
+      });
+
+      const spendingCondition = tx.auth.spendingCondition as StxTx.MultiSigSpendingCondition;
+      it(`Tx ${i} should have correct pubkeys`, () => {
+        spendingCondition.fields.forEach((f, i) => {
+          expect(f.contents.type).toEqual(StxTx.StacksMessageType.PublicKey);
+          const pubkey = f.contents.data.toString('hex');
+          expect(pubkey).toEqual(input.publicKeys[i]);
+        });
+      });
+
+      it(`Tx ${i} should have correct fee, nonce, and hash mode`, () => {
+        expect(spendingCondition.fee).toEqual(BigInt(input.fee));
+        expect(spendingCondition.hashMode).toEqual(StxTx.AddressHashMode.SerializeP2SH);
+        if (input.nonce) {
+          expect(spendingCondition.nonce).toEqual(BigInt(input.nonce));
+        }
+      });
+    }
+  });
 });
